ff00b1309da250595e76360136e9bbb0b427e16a
albertigno <albertigno1129@gmail.com>
2024-11-04 15:11:23 +0100

    another commit

--- model_test.py
+++ model_test.py
@@ -5,18 +5,20 @@ from snn_delays.utils.dataset_loader import DatasetLoader
 from snn_delays.utils.train_utils import train, get_device
 from snn_delays.utils.test_behavior import tb_save_max_last_acc
 
-device = get_device()
+#device = get_device()
+
+device = 'cpu'
 
 # for reproducibility
 torch.manual_seed(10)
 
 dataset = 'shd'
-total_time = 64
-batch_size = 256  # lr=1e-4
+total_time = 30
+batch_size = 1024  # lr=1e-4
 
 # DATASET
 DL = DatasetLoader(dataset=dataset,
-                   caching='memory',
+                   caching='disk',
                    num_workers=0,
                    batch_size=batch_size,
                    total_time=total_time)
@@ -27,13 +29,39 @@ taimu1 = time.time()
 
 tau_m = 3.0
 
-snn = SNN(dataset_dict=dataset_dict, structure=(1048, 2), connection_type='f',
-          delay=(64, 1), delay_type='ho', tau_m = tau_m,
+snn1 = SNN(dataset_dict=dataset_dict, structure=(48, 2), connection_type='f',
+          delay=(10, 1), delay_type='ho', tau_m = tau_m,
           win=total_time, loss_fn='mem_sum', batch_size=batch_size, device=device,
           debug=True)
 
-snn.to(device)
+snn2 = SNN(dataset_dict=dataset_dict, structure=(48, 2), connection_type='f',
+          delay=None, delay_type='', tau_m = tau_m,
+          win=total_time, loss_fn='mem_sum', batch_size=batch_size, device=device,
+          debug=True)
+
+snn3 = SNN(dataset_dict=dataset_dict, structure=(48, 2), connection_type='r',
+          delay=(10, 1), delay_type='ho', tau_m = tau_m,
+          win=total_time, loss_fn='mem_sum', batch_size=batch_size, device=device,
+          debug=True)
+
+snn4 = SNN(dataset_dict=dataset_dict, structure=(48, 2), connection_type='r',
+          delay=None, delay_type='', tau_m = tau_m,
+          win=total_time, loss_fn='mem_sum', batch_size=batch_size, device=device,
+          debug=True)
+.
+snn1.to(device)
+snn2.to(device)
+snn3.to(device)
+snn4.to(device)
+
 ckpt_dir = 'exp_default'  # donde se guardará
-train(snn, train_loader, test_loader, 1e-3, 50, dropout=0.0, lr_scale=(5.0, 2.0), 
+train(snn1, train_loader, test_loader, 4*1e-3, 5, dropout=0.0, lr_scale=(5.0, 2.0), 
       test_behavior=tb_save_max_last_acc, ckpt_dir=ckpt_dir, scheduler=(10, 0.95), test_every=5, lsm=True)
+train(snn2, train_loader, test_loader, 4*1e-3, 5, dropout=0.0, lr_scale=(5.0, 2.0), 
+      test_behavior=tb_save_max_last_acc, ckpt_dir=ckpt_dir, scheduler=(10, 0.95), test_every=5, lsm=True)
+train(snn3, train_loader, test_loader, 4*1e-3, 5, dropout=0.0, lr_scale=(5.0, 2.0), 
+      test_behavior=tb_save_max_last_acc, ckpt_dir=ckpt_dir, scheduler=(10, 0.95), test_every=5, lsm=True)
+train(snn4, train_loader, test_loader, 4*1e-3, 5, dropout=0.0, lr_scale=(5.0, 2.0), 
+      test_behavior=tb_save_max_last_acc, ckpt_dir=ckpt_dir, scheduler=(10, 0.95), test_every=5, lsm=True)
+
 print('[INFO] TIEMPO: ', time.time() - taimu1)
