4f1eefb60be856d2a0411e6b6ba40b016d49d5dd
albertigno <albertigno1129@gmail.com>
2024-12-03 11:16:07 +0100

    multilayer stmnist

--- experiments/exp12_gestures_random_crop_time.py
+++ experiments/exp12_gestures_random_crop_time.py
@@ -23,189 +23,193 @@ rpts = 3
 
 for rcrop in [1e5, 7e5, 3e6]:
 
-    # DATASET
-    DL = DatasetLoader(dataset=dataset,
-                    caching='memory',
-                    num_workers=0,
-                    batch_size=batch_size,
-                    total_time=total_time,
-                    sensor_size_to=64,
-                    random_crop_to=(3e6, rcrop))
-    train_loader, test_loader, dataset_dict = DL.get_dataloaders()
+    for test_cfg in ['randomtest', 'fixedtest']:
 
-    # ### make test set fixed for reproducible results...? (hopefully)
-    # DL = DatasetLoader(dataset=dataset,
-    #                 caching='memory',
-    #                 num_workers=0,
-    #                 batch_size=batch_size,
-    #                 total_time=total_time,
-    #                 sensor_size_to=64,
-    #                 crop_to=rcrop)
-    # _, test_loader, dataset_dict = DL.get_dataloaders()
+        # DATASET
+        DL = DatasetLoader(dataset=dataset,
+                        caching='memory',
+                        num_workers=0,
+                        batch_size=batch_size,
+                        total_time=total_time,
+                        sensor_size_to=64,
+                        random_crop_to=(3e6, rcrop))
+        train_loader, test_loader, dataset_dict = DL.get_dataloaders()
 
-    ### fixed params
+        if test_cfg == 'fixedtest':
 
-    model_params = {'dataset_dict': dataset_dict, 'delay_type':'h',
-                    'reset_to_zero':True, 'win':total_time,
-                    'loss_fn':'mem_sum', 'batch_size':batch_size, 'device':device,
-                    'debug':False}
+            ### make test set fixed for reproducible results...? (hopefully)
+            DL = DatasetLoader(dataset=dataset,
+                            caching='memory',
+                            num_workers=0,
+                            batch_size=batch_size,
+                            total_time=total_time,
+                            sensor_size_to=64,
+                            crop_to=rcrop)
+            _, test_loader, _ = DL.get_dataloaders()
 
-    ckpt_dir = 'exp12_gestures_random_crop_time'
+        ### fixed params
 
-    train_params = {'learning_rate':1e-3, 'num_epochs':50, 'spk_reg':0.0, 'l1_reg':0.0,
-            'dropout':0.0, 'lr_tau': 0.1, 'scheduler':(10, 0.95), 'ckpt_dir':ckpt_dir,
-            'test_behavior':tb_save_max_last_acc, 'test_every':1, 'delay_pruning':None, 'weight_pruning':None,
-            'lsm':False, 'random_delay_pruning' : None, 'weight_quantization': None, 'k':None, 'depth': None, 'verbose':True}
+        model_params = {'dataset_dict': dataset_dict, 'delay_type':'h',
+                        'reset_to_zero':True, 'win':total_time,
+                        'loss_fn':'mem_sum', 'batch_size':batch_size, 'device':device,
+                        'debug':False}
 
-    #### first run (f+d)
-    sweep_params = {
-        'connection_type': ['f'],
-        'delay': [(48*2, 16*2)],
-        'structure':[(64,2)],
-        'tau_m':[20.0, 'normal'],
-        'T_freeze_taus':[True, None]
-        }
+        ckpt_dir = 'exp12_gestures_random_crop_time'
 
-    sweep_params_names = {
-        'connection_type': ['f'],
-        'delay': ['d'],
-        'structure':['2l'],
-        'tau_m':['hm', 'ht'],
-        'T_freeze_taus':['ft', 'tt']
-        }
+        train_params = {'learning_rate':1e-3, 'num_epochs':50, 'spk_reg':0.0, 'l1_reg':0.0,
+                'dropout':0.0, 'lr_tau': 0.1, 'scheduler':(10, 0.95), 'ckpt_dir':ckpt_dir,
+                'test_behavior':tb_save_max_last_acc, 'test_every':1, 'delay_pruning':None, 'weight_pruning':None,
+                'lsm':False, 'random_delay_pruning' : None, 'weight_quantization': None, 'k':None, 'depth': None, 'verbose':True}
 
-    import itertools
-    def get_configs(sweep_params, sweep_params_names):
-        configurations = list(itertools.product(*sweep_params.values()))
-        configurations_names = list(itertools.product(*sweep_params_names.values()))
-        all_configs = []
-        for config, config_names in zip(configurations, configurations_names):
-            config_dict = dict(zip(sweep_params.keys(), config))
-            config_dict['name']='_'.join(list(config_names))
-            all_configs.append(config_dict)
-        return all_configs
+        #### first run (f+d)
+        sweep_params = {
+            'connection_type': ['f'],
+            'delay': [(48*2, 16*2)],
+            'structure':[(64,2)],
+            'tau_m':['normal'],
+            'T_freeze_taus':[None]
+            }
 
-    cfgs = get_configs(sweep_params, sweep_params_names)
+        sweep_params_names = {
+            'connection_type': ['f'],
+            'delay': ['d'],
+            'structure':['2l'],
+            'tau_m':['ht'],
+            'T_freeze_taus':['tt']
+            }
 
-    num_repetitions = rpts
-    for cfg in cfgs:
-        for repetition in range(0, num_repetitions):
-            for key, value in zip(cfg.keys(), cfg.values()):
-                if key != 'name':
-                    if key.split('_')[0]=='T':
-                        train_params[key[2:]] = value
-                    else:
-                        model_params[key] = value
+        import itertools
+        def get_configs(sweep_params, sweep_params_names):
+            configurations = list(itertools.product(*sweep_params.values()))
+            configurations_names = list(itertools.product(*sweep_params_names.values()))
+            all_configs = []
+            for config, config_names in zip(configurations, configurations_names):
+                config_dict = dict(zip(sweep_params.keys(), config))
+                config_dict['name']='_'.join(list(config_names))
+                all_configs.append(config_dict)
+            return all_configs
 
-            print('-----NEW TRAINING-------')
-            print(model_params)
-            print(train_params)
+        cfgs = get_configs(sweep_params, sweep_params_names)
 
-            snn = SNN(**model_params)
-            snn.input2spike_th = None 
+        num_repetitions = rpts
+        for cfg in cfgs:
+            for repetition in range(0, num_repetitions):
+                for key, value in zip(cfg.keys(), cfg.values()):
+                    if key != 'name':
+                        if key.split('_')[0]=='T':
+                            train_params[key[2:]] = value
+                        else:
+                            model_params[key] = value
 
-            snn.model_name = 'cr'+str(rcrop)+'_'+cfg['name'] + '_rpt' + str(repetition)
-            snn.save_model(snn.model_name + "_initial", ckpt_dir)
+                print('-----NEW TRAINING-------')
+                print(model_params)
+                print(train_params)
 
-            train(snn, train_loader, test_loader, **train_params)
+                snn = SNN(**model_params)
+                snn.input2spike_th = None 
 
-    #### second run (r)
-    sweep_params = {
-        'connection_type': ['r'],
-        'delay': [None],
-        'structure':[(64,2)],
-        'tau_m':[20.0, 'normal'],
-        'T_freeze_taus':[True, None]
-        }
+                snn.model_name = test_cfg+str(rcrop)+'_'+cfg['name'] + '_rpt' + str(repetition)
+                snn.save_model(snn.model_name + "_initial", ckpt_dir)
 
-    sweep_params_names = {
-        'connection_type': ['r'],
-        'delay': ['nd'],
-        'structure':['2l'],
-        'tau_m':['hm', 'ht'],
-        'T_freeze_taus':['ft', 'tt']
-        }
+                train(snn, train_loader, test_loader, **train_params)
 
-    import itertools
-    def get_configs(sweep_params, sweep_params_names):
-        configurations = list(itertools.product(*sweep_params.values()))
-        configurations_names = list(itertools.product(*sweep_params_names.values()))
-        all_configs = []
-        for config, config_names in zip(configurations, configurations_names):
-            config_dict = dict(zip(sweep_params.keys(), config))
-            config_dict['name']='_'.join(list(config_names))
-            all_configs.append(config_dict)
-        return all_configs
+        #### second run (r)
+        sweep_params = {
+            'connection_type': ['r'],
+            'delay': [None],
+            'structure':[(64,2)],
+            'tau_m':['normal'],
+            'T_freeze_taus':[None]
+            }
 
-    cfgs = get_configs(sweep_params, sweep_params_names)
+        sweep_params_names = {
+            'connection_type': ['r'],
+            'delay': ['nd'],
+            'structure':['2l'],
+            'tau_m':['ht'],
+            'T_freeze_taus':['tt']
+            }
 
-    num_repetitions = rpts
-    for cfg in cfgs:
-        for repetition in range(0, num_repetitions):
-            for key, value in zip(cfg.keys(), cfg.values()):
-                if key != 'name':
-                    if key.split('_')[0]=='T':
-                        train_params[key[2:]] = value
-                    else:
-                        model_params[key] = value
+        import itertools
+        def get_configs(sweep_params, sweep_params_names):
+            configurations = list(itertools.product(*sweep_params.values()))
+            configurations_names = list(itertools.product(*sweep_params_names.values()))
+            all_configs = []
+            for config, config_names in zip(configurations, configurations_names):
+                config_dict = dict(zip(sweep_params.keys(), config))
+                config_dict['name']='_'.join(list(config_names))
+                all_configs.append(config_dict)
+            return all_configs
 
-            print('-----NEW TRAINING-------')
-            print(model_params)
-            print(train_params)
+        cfgs = get_configs(sweep_params, sweep_params_names)
 
-            snn = SNN(**model_params)
-            snn.model_name = 'cr'+str(rcrop)+'_'+cfg['name'] + '_rpt' + str(repetition)
-            snn.save_model(snn.model_name + "_initial", ckpt_dir)
+        num_repetitions = rpts
+        for cfg in cfgs:
+            for repetition in range(0, num_repetitions):
+                for key, value in zip(cfg.keys(), cfg.values()):
+                    if key != 'name':
+                        if key.split('_')[0]=='T':
+                            train_params[key[2:]] = value
+                        else:
+                            model_params[key] = value
 
-            train(snn, train_loader, test_loader, **train_params)
+                print('-----NEW TRAINING-------')
+                print(model_params)
+                print(train_params)
+
+                snn = SNN(**model_params)
+                snn.model_name = test_cfg+str(rcrop)+'_'+cfg['name'] + '_rpt' + str(repetition)
+                snn.save_model(snn.model_name + "_initial", ckpt_dir)
+
+                train(snn, train_loader, test_loader, **train_params)
 
 
-    #### third run (f)
-    sweep_params = {
-        'connection_type': ['f'],
-        'delay': [None],
-        'structure':[(64,4)],
-        'tau_m':[20.0, 'normal'],
-        'T_freeze_taus':[True, None]
-        }
+        #### third run (f)
+        sweep_params = {
+            'connection_type': ['f'],
+            'delay': [None],
+            'structure':[(64,4)],
+            'tau_m':['normal'],
+            'T_freeze_taus':[None]
+            }
 
-    sweep_params_names = {
-        'connection_type': ['f'],
-        'delay': ['nd'],
-        'structure':['4l'],
-        'tau_m':['hm', 'ht'],
-        'T_freeze_taus':['ft', 'tt']
-        }
+        sweep_params_names = {
+            'connection_type': ['f'],
+            'delay': ['nd'],
+            'structure':['4l'],
+            'tau_m':['ht'],
+            'T_freeze_taus':['tt']
+            }
 
-    import itertools
-    def get_configs(sweep_params, sweep_params_names):
-        configurations = list(itertools.product(*sweep_params.values()))
-        configurations_names = list(itertools.product(*sweep_params_names.values()))
-        all_configs = []
-        for config, config_names in zip(configurations, configurations_names):
-            config_dict = dict(zip(sweep_params.keys(), config))
-            config_dict['name']='_'.join(list(config_names))
-            all_configs.append(config_dict)
-        return all_configs
+        import itertools
+        def get_configs(sweep_params, sweep_params_names):
+            configurations = list(itertools.product(*sweep_params.values()))
+            configurations_names = list(itertools.product(*sweep_params_names.values()))
+            all_configs = []
+            for config, config_names in zip(configurations, configurations_names):
+                config_dict = dict(zip(sweep_params.keys(), config))
+                config_dict['name']='_'.join(list(config_names))
+                all_configs.append(config_dict)
+            return all_configs
 
-    cfgs = get_configs(sweep_params, sweep_params_names)
+        cfgs = get_configs(sweep_params, sweep_params_names)
 
-    num_repetitions = rpts
-    for cfg in cfgs:
-        for repetition in range(0, num_repetitions):
-            for key, value in zip(cfg.keys(), cfg.values()):
-                if key != 'name':
-                    if key.split('_')[0]=='T':
-                        train_params[key[2:]] = value
-                    else:
-                        model_params[key] = value
+        num_repetitions = rpts
+        for cfg in cfgs:
+            for repetition in range(0, num_repetitions):
+                for key, value in zip(cfg.keys(), cfg.values()):
+                    if key != 'name':
+                        if key.split('_')[0]=='T':
+                            train_params[key[2:]] = value
+                        else:
+                            model_params[key] = value
 
-            print('-----NEW TRAINING-------')
-            print(model_params)
-            print(train_params)
+                print('-----NEW TRAINING-------')
+                print(model_params)
+                print(train_params)
 
-            snn = SNN(**model_params)
-            snn.model_name = 'cr'+str(rcrop)+'_'+cfg['name'] + '_rpt' + str(repetition)
-            snn.save_model(snn.model_name + "_initial", ckpt_dir)
+                snn = SNN(**model_params)
+                snn.model_name = test_cfg+str(rcrop)+'_'+cfg['name'] + '_rpt' + str(repetition)
+                snn.save_model(snn.model_name + "_initial", ckpt_dir)
 
-            train(snn, train_loader, test_loader, **train_params)
+                train(snn, train_loader, test_loader, **train_params)
--- experiments/snn2chip/train_stmnist_base.py
+++ experiments/snn2chip/train_stmnist_base.py
@@ -26,7 +26,32 @@ DL = DatasetLoader(dataset=dataset,
 
 train_loader, test_loader, dataset_dict = DL.get_dataloaders()
 
-structure = (64, 1)
+structure = (64, 3)
+
+dataset_dict["time_ms"] = 2e3
+
+loss_fn = 'mem_sum'
+
+snn = SNN(dataset_dict=dataset_dict, structure=structure, connection_type='f',
+          delay=None, delay_type='', tau_m='normal',
+          reset_to_zero=True, win=total_time,
+          loss_fn=loss_fn, batch_size=batch_size, device=device,
+          debug=True)
+
+snn.set_network()
+snn.to(device)
+
+snn.model_name = f'full_weights_{structure}' + snn.model_name
+
+num_epochs = 100
+lr = 1e-3
+ckpt_dir = 'exp_snn2chip'
+
+train(snn, train_loader, test_loader, lr, num_epochs, dropout=0.0, 
+    test_behavior=tb_save_max_last_acc, ckpt_dir=ckpt_dir, scheduler=(10, 0.95), test_every=1, freeze_taus=True)
+
+
+structure = (64, 4)
 
 dataset_dict["time_ms"] = 2e3
 
