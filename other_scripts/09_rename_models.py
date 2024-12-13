29b8e6e98ade010042b028fe2e3e893025ad9a71
albertigno <albertigno1129@gmail.com>
2024-12-13 17:02:29 +0100

    updated results

--- other_scripts/09_rename_models.py
+++ other_scripts/09_rename_models.py
@@ -1,15 +1,19 @@
 import os
 
 # Source directory
-source_dir = r'C:\Users\Alberto\checkpoints\abl1_shd50_rnn'
+source_dir = r'C:\Users\Alberto\abl1_shd50_rnn_8'
 
 # Iterate through all files in the directory
 for filename in os.listdir(source_dir):
     # Check if the filename contains '_rpt' 
     if '_rpt' in filename:
         # Create new filename with replacements
-        new_filename = filename.replace('_rpt0_', '_rpt8_').replace('_rpt1_', '_rpt9_')
-        
+        new_filename = filename.replace('_rpt0_', '_rpt5_')
+        new_filename = new_filename.replace('_rpt1_', '_rpt6_')
+        new_filename = new_filename.replace('_rpt2_', '_rpt7_')
+        new_filename = new_filename.replace('_rpt3_', '_rpt8_')
+        new_filename = new_filename.replace('_rpt4_', '_rpt9_')
+      
         # Construct full file paths
         old_path = os.path.join(source_dir, filename)
         new_path = os.path.join(source_dir, new_filename)
